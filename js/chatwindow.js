6c1fbb2501de6607a418f9b0f1334d2bffc1a0e5
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-07-14 13:46:31 +0530

    initial bringup

---
+++ js/chatwindow.js
@@ -0,0 +1,34 @@
+function dragMove(){
+    overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success" && result.window.state !== "Maximized"){
+        overwolf.windows.dragMove(result.window.id);
+        }
+    });
+};
+
+async function send_msg(msg) {
+    if(event.key === 'Enter') {
+        // audio device setup
+        //const devices = await navigator.mediaDevices.enumerateDevices();
+        //const audioDevices = devices.filter(devices => devices.kind == 'audiooutput');
+        //const audio = document.createElement('audio');
+        //await audio.setSinkId(audioDevices[0].deviceId);
+        //console.log('Audio is being played on ' + audio.sinkId);
+
+        var tts = new SpeechSynthesisUtterance();
+        var tts_voices = window.speechSynthesis.getVoices();
+        tts.voice = tts_voices[10];
+        tts.voiceURI = 'native';
+        tts.volume = 1;
+        tts.rate = 1;
+        tts.pitch = 2;
+        tts.text = msg.value;
+        tts.lang = 'en-IN';
+
+        msg.onend = function(e) {
+            console.log('Finished in ' + event.elapsedTime + ' seconds.');
+        };
+          
+        speechSynthesis.speak(tts);
+    }
+}
